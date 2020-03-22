5fd10e44a919407bcc31e143780c067916a19a2e
Daniel Wang <hello@danielwang.dev>
2020-03-21 21:21:10 -0400

    refactor: simplify namespaces

--- csgo-internal/core/hooks/hooked/wndproc.cpp
+++ csgo-internal/core/hooks/hooked/wndproc.cpp
@@ -10,39 +10,39 @@ namespace cheat::core::hooks::wndproc {
 		switch (message)
 		{
 		case WM_LBUTTONDOWN:
-			cheat::sdk::misc::input::pressed[VK_LBUTTON] = true;
+			sdk::misc::input::pressed[VK_LBUTTON] = true;
 			break;
 		case WM_LBUTTONUP:
-			cheat::sdk::misc::input::pressed[VK_LBUTTON] = false;
+			sdk::misc::input::pressed[VK_LBUTTON] = false;
 			break;
 		case WM_RBUTTONDOWN:
-			cheat::sdk::misc::input::pressed[VK_RBUTTON] = true;
+			sdk::misc::input::pressed[VK_RBUTTON] = true;
 			break;
 		case WM_RBUTTONUP:
-			cheat::sdk::misc::input::pressed[VK_RBUTTON] = false;
+			sdk::misc::input::pressed[VK_RBUTTON] = false;
 			break;
 		case WM_MBUTTONDOWN:
-			cheat::sdk::misc::input::pressed[VK_MBUTTON] = true;
+			sdk::misc::input::pressed[VK_MBUTTON] = true;
 			break;
 		case WM_MBUTTONUP:
-			cheat::sdk::misc::input::pressed[VK_MBUTTON] = false;
+			sdk::misc::input::pressed[VK_MBUTTON] = false;
 			break;
 		case WM_XBUTTONDOWN:
 		case WM_XBUTTONUP:
 		{
 			auto button = GET_XBUTTON_WPARAM(wparam);
 			if (button == XBUTTON1)
-				cheat::sdk::misc::input::pressed[VK_XBUTTON1] = (message == WM_XBUTTONDOWN);
+				sdk::misc::input::pressed[VK_XBUTTON1] = (message == WM_XBUTTONDOWN);
 			else if (button == XBUTTON2)
-				cheat::sdk::misc::input::pressed[VK_XBUTTON2] = (message == WM_XBUTTONDOWN);
+				sdk::misc::input::pressed[VK_XBUTTON2] = (message == WM_XBUTTONDOWN);
 		}
 		break;
 		case WM_KEYDOWN:
-			cheat::sdk::misc::input::pressed[wparam] = true;
+			sdk::misc::input::pressed[wparam] = true;
 			break;
 		case WM_KEYUP:
-			cheat::sdk::misc::input::pressed[wparam] = false;
-			cheat::sdk::misc::input::handle(wparam);
+			sdk::misc::input::pressed[wparam] = false;
+			sdk::misc::input::handle(wparam);
 			break;
 		default:
 			break;
@@ -51,6 +51,6 @@ namespace cheat::core::hooks::wndproc {
 		if (menu::visible && ImGui_ImplWin32_WndProcHandler(window, message, wparam, lparam))
 			return true;
 
-		return CallWindowProc(cheat::core::hooks::wndproc::original, window, message, wparam, lparam);
+		return CallWindowProc(original, window, message, wparam, lparam);
 	}
 }
